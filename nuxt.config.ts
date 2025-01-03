1438cde8b02e13c9cc82243537b0c5412b4572c7
webchillgames <web.chill.games@gmail.com>
2025-01-03 14:47:44 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.NODE_ENV === "production" ? "/hookahly-talks/" : "/",
-  },
+  // app: {
+  //   baseURL: process.env.NODE_ENV === "production" ? "/hookahly-talks/" : "/",
+  // },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   modules: ["@vite-pwa/nuxt"],
